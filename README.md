8e68ed49d2d3c83c77ebd557472780a26a6dab7e
evicno <229501549+evicno@users.noreply.github.com>
2025-12-27 21:12:55 +0100

    Add a restart button

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # odin-tic-tac-toe
 Tic Tac Toe project from The Odin Project
+
+Image by rawpixel.com on [Freepik](href="https://www.freepik.com/free-photo/painted-solid-concrete-wall-textured-backdrop_17592909.htm")
+
--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Create the structure of the gameboard
 const gameboard = (function() {
     const rows = 3;
     const columns = 3;
@@ -30,27 +31,19 @@ const gameboard = (function() {
         }
     })();
 
+    // Access a cell of the board array from its index
     function getCell(index) {
         let i = Math.floor(index / 3);
         let j = index % 3;
         return board[i][j];
     }
 
-    function getBoard() {
-        for (let i = 0; i < rows; i++) {
-                let cell1 = getCell(i * 3).getValue();
-                let cell2 = getCell(i * 3 + 1).getValue();
-                let cell3 = getCell(i * 3 + 2).getValue();
-                console.log(cell1, cell2, cell3);
-        };
-    }
-
-    return {board, cell, getCell, getBoard};
+    return {board, cell, getCell};
 
 })();
 
-
-function player(name, marker) {
+// Create a player objects and functions to get/set data
+function player(name, marker, score = 0) {
     let moves = [];
 
     const getName = () => {
@@ -70,12 +63,31 @@ function player(name, marker) {
             gameboard.getCell(move).setValue(marker);
     }
 
-    return {getName, setName, marker, getMoves, addMove};
+    const clearMoves = () => {
+        moves = [];
+        return moves;
+    }
+
+    const getScore = () => {
+        return score;
+    }
+
+    const addPoint = () => {
+        score += 1;
+        return score;
+    }
+
+    return {
+        getName, setName, 
+        marker, 
+        getMoves, addMove, clearMoves,
+        getScore, addPoint};
 }
 
+// Control the flow of the game
 const gameController = (function() {
-    const playerOne = player("playerOne", "O");
-    const playerTwo = player("playerTwo", "X");
+    const playerOne = player("Player 1", "O");
+    const playerTwo = player("Player 2", "X");
     let currentPlayer = playerOne;
     let turns = 0;
 
@@ -83,16 +95,22 @@ const gameController = (function() {
         return currentPlayer;
     }
 
+    const clearGame = () => {
+        turns = 0;
+        currentPlayer = playerOne;
+        return {turns, currentPlayer}
+    }
+
     // Check if the move played has not been played yet.
     // If not, add move index to player moves and change cell's value.
     function playRound(player, move) {
         currentPlayer.addMove(move);
-        gameboard.getBoard();
         turns += 1;
         checkWin();
         switchPlayer();
     }
 
+    // Switch player after each round
     function switchPlayer() {
         if (currentPlayer === playerOne) {
             currentPlayer = playerTwo;
@@ -115,6 +133,8 @@ const gameController = (function() {
             ["2", "4", "6"]
         ]
         let count = 0;
+        // Loop through the winners combinations to check if all 3 numbers are included
+        // in current player's moves
         for (const winner of winners) {
             count = 0;
             for (const num of winner) {
@@ -126,27 +146,37 @@ const gameController = (function() {
                 }
             }
             if (count == 3) {
-                console.log(currentPlayer.getName() + " wins!");
+                endGame(currentPlayer);
                 return;
             }
         }
+        // If all squares(9) have been played, it's a tie game
         if (turns === 9) {
         console.log("Tie game!");
         return;
         }
     };
 
-    gameboard.getBoard();
-    return {playerOne, playerTwo, getCurrentPlayer, playRound};
+    return {playerOne, playerTwo, getCurrentPlayer, clearGame, playRound};
     
 })();
 
-
+// Display the game, handle buttons
 function displayController() {
     const squares = document.getElementsByClassName("square");
-    const playerOneName = document.querySelector(".one .name");
-    const playerTwoName = document.querySelector(".two .name");
 
+    const playerOneName = document.querySelector(".one .name");
+    const playerOneMarker = document.querySelector(".one .marker");
+    const playerOneScore = document.querySelector(".one .score");
+
+    const playerTwoName = document.querySelector(".two .name");
+    const playerTwoMarker = document.querySelector(".two .marker");
+    const playerTwoScore = document.querySelector(".two .score");
+
+    const startButton = document.querySelector(".start");
+    const changeButtons = document.querySelectorAll(".name-button");
+
+    // Change name of the player (after button clicked)
     function changeName(playerOld, playerName) {
         let newName = prompt("New name for " + playerOld.getName());
         if (newName) {
@@ -158,22 +188,85 @@ function displayController() {
         }
     }
 
+    // Start the game
+    function startGame() {
+        // Remove change name buttons
+        changeButtons.forEach((button) => {
+            button.remove();
+        });
+        getMarkers();
+        clearGameboard();
+
+        //Set scores to 0;
+        // playerOneScore.textContent = gameController.playerOne.getScore();
+        // playerTwoScore.textContent = gameController.playerTwo.getScore()
+
+        // Create the grid buttons
+        const squareButtons = document.querySelectorAll(".square-button");
+        squareButtons.forEach((button) => {
+            button.addEventListener("click", () => {
+                // Add marker of the current player in the square
+                button.textContent = gameController.getCurrentPlayer().marker;
+                // Disable button to avoid unauthorized move
+                button.disabled = true;
+                gameController.playRound(gameController.getCurrentPlayer(), button.dataset.id);
+                getMarkers();
+
+            })
+        });
+
+        // Add a restart button
+        const restartButton = document.createElement("button");
+        const header = document.querySelector(".header");
+        restartButton.textContent = "Restart";
+        header.appendChild(restartButton); 
+        restartButton.addEventListener("click", () => {
+            clearGameboard();
+            
+        })
+        
+    }
+
+    // Display marker of currentPlayer while hiding the other player's marker
+    function getMarkers() {
+        // player one to play
+        if (gameController.getCurrentPlayer().marker == "O") { 
+            playerOneMarker.textContent = "O";
+            playerTwoMarker.textContent = "";
+        }
+        // player two to play
+        else {
+            playerOneMarker.textContent = "";
+            playerTwoMarker.textContent = "X";
+        }
+    }
+
+    function clearGameboard() {
+        // Clear grid
+        let squareButtons = document.querySelectorAll(".square-button");
+        for (let i = 0; i < squareButtons.length; i++) {
+            squareButtons[i].textContent = "";
+            squareButtons[i].disabled = false;
+        }
+        // Clear player's moves
+        gameController.playerOne.clearMoves();
+        gameController.playerTwo.clearMoves();
+
+        // Clear moves (to check ties) and set currentPlayer to player 1
+        gameController.clearGame();
+        getMarkers();
+    }
+
     function renderGameboard() {
+        // Render gameboard with actualised data
         for (let i = 0; i < squares.length; i++) {
             let button = document.createElement("button");
             button.classList.add("square-button");
             button.dataset.id = i;
             squares[i].appendChild(button);
         }
-        const squareButtons = document.querySelectorAll(".square-button");
-        squareButtons.forEach((button) => {
-            button.addEventListener("click", () => {
-                button.textContent = gameController.getCurrentPlayer().marker;
-                button.disabled = true;
-                gameController.playRound(gameController.getCurrentPlayer(), button.dataset.id);
-            });
-        });
-        const changeButtons = document.querySelectorAll(".name-button");
+        
+        // Change name of the player (js and DOM)
         changeButtons.forEach((button) => {
             button.addEventListener("click", () => {
                 if (button.id == "change-one") {
@@ -183,6 +276,11 @@ function displayController() {
                     changeName(gameController.playerTwo, playerTwoName);
                 }
             })
+        })
+
+        startButton.addEventListener("click", () => {
+            startButton.remove();
+            startGame();
         })
     }
     
